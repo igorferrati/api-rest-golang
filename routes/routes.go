e6a5ffe90bac608ef0bf7aa19468656cbeacff6a
igorferrati <ferrati.igor@gmail.com>
2023-12-26 21:21:56 -0300

    add middleware

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/igorferrati/api-rest-golang/controllers"
+	"github.com/igorferrati/api-rest-golang/middleware"
 )
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContenTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("Get")
